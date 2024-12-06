cbac937a982fd1da024d0bf5af9892a94b07ed0f
C. Michael Pilato <cmpilato@digital.ai>
2024-12-06 09:01:00 -0500

    Fix lint complaints

--- thotkeeper/utils.py
+++ thotkeeper/utils.py
@@ -13,7 +13,9 @@ from requests.exceptions import HTTPError
 from .version import (__version__, parse_version)
 
 
-LATEST_VERSION_URL = 'https://raw.githubusercontent.com/cmpilato/thotkeeper/refs/heads/master/www/latest-version.json'
+LATEST_VERSION_URL = ('https://raw.githubusercontent.com/cmpilato/thotkeeper/'
+                      'refs/heads/master/www/latest-version.json')
+
 
 def update_check(update_url=LATEST_VERSION_URL):
     """Consult the contents of a web-accessible JSON file for the
